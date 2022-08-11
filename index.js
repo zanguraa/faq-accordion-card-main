4b5ecd5d34c9a248dd0ff9d1edce632fa3eb8a8a
Valeri Zanguri <zanguraa@gmail.com>
2022-08-11 17:34:23 +0400

    added function wich close other tags, when one is opened

--- index.js
+++ index.js
@@ -4,13 +4,31 @@ const spanEl = document.getElementsByClassName('hide-span');
 const arrowImg = document.getElementsByClassName('img-arrow');
 const trueSpan = document.getElementById('span-2');
     
+let same = false;
 for(let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener('click', ()=> { 
-    acc[i].classList.toggle('h4-bold');
-    acc[i].parentElement.parentElement.lastElementChild.classList.toggle('show-span');
-    acc[i].nextElementSibling.classList.toggle('arrow-rotate-img');
+    acc[i].addEventListener('click', (event)=> { 
+        let findActive = document.querySelector('.active');
+        let h4;
+        if(findActive) {
+            h4 = findActive.firstElementChild.firstElementChild;
+        }
+        console.log(h4);
+        if(h4 == acc[i]) {
+           findActive.classList.toggle('active');
+        } else {
+            findActive?.classList.remove('active');
+            acc[i].parentElement.parentElement.classList.toggle('active');
+
+        }
+
+        
+    
     });
+    
+    
 }
+
+
 
 
     
